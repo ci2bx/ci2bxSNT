1717085b690a1f0e5e60230fb30983b97b8c2757
ci2bx <charles.issert2braux@gmail.com>
2023-12-16 16:57:09 +0100

    padding mobile

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if (window.innerHeight>window.innerWidth){
+    document.getElementById("content").className = "paspadding"
+}
+
 function deathscreen(){
     document.getElementById("content").className = "death_screen"
     document.getElementById("content").innerHTML = "<link rel='stylesheet' href='style.css'><smilet class=death_screen >:(</smilet> <br><br><p class=death_screen>Your PC ran into a problem. Plase wait a few seconds...<br>If this page is frozen, please call this number : 01 48 42 48</p><audio autoplay preload><source src=windows_10.mp3 type=audio/mp3 /></audio>";
